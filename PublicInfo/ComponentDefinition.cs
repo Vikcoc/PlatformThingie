4074c7a0d2000d3ba62d4c06901f78af655e844a
Victor Cociobanu <vikcoc@yahoo.com>
2024-03-24 21:20:06 +0200

    Prep work for user page

--- PublicInfo/ComponentDefinition.cs
+++ PublicInfo/ComponentDefinition.cs
@@ -29,6 +29,8 @@ namespace PublicInfo
                 () => Results.File(Path.Combine(Directory.GetCurrentDirectory(), "pages", "all", "filled-tonal-button.js"), "text/javascript"));
             endpoints.MapGet("/public/back-logo",
                 () => Results.File(Path.Combine(Directory.GetCurrentDirectory(), "pages", "all", "back-logo.svg"), "image/svg+xml"));
+            endpoints.MapGet("/public/topbar/signed-in",
+                () => Results.File(Path.Combine(Directory.GetCurrentDirectory(), "pages", "all", "signed-in.js"), "text/javascript"));
 
             //the index i guess
             endpoints.MapGet("/", () => Results.Redirect("/about"));
---
+++ UserInfo/ComponentDefinition.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.DependencyInjection;
+using PlatformInterfaces;
+
+namespace UserInfo
+{
+    public class ComponentDefinition : IPlatformComponentDefinition
+    {
+        public string GivenName => "User";
+
+        public void AddServices(IServiceCollection services)
+        {
+        }
+
+        public void AddRoutes(IEndpointRouteBuilder endpoints)
+        {
+
+        }
+    }
+}
