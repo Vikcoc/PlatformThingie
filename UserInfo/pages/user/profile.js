85a74ba879f174612b9935028b89f23574f242a8
Victor-Andrei Cociobanu <v.cociobanu@lipton.com>
2024-03-29 16:57:20 +0200

    prep

--- UserInfo/pages/user/profile.js
+++ UserInfo/pages/user/profile.js
@@ -1,4 +1,54 @@
 import { authenticatedFetch } from '/public/authenticated-fetch';
+
+function getEditableField(claim) {
+    var sec = document.createElement("section");
+    sec.classList.add("horizontalLine");
+    var sel = document.createElement("md-outlined-select");
+
+    var opt = document.createElement("md-select-option");
+    opt.textContent = "";
+    sel.appendChild(opt);
+    var opt = document.createElement("md-select-option");
+    opt.textContent = "Yes";
+    sel.appendChild(opt);
+    var opt = document.createElement("md-select-option");
+    opt.textContent = "No";
+    sel.appendChild(opt);
+
+    var inp = document.createElement("md-outlined-text-field");
+    inp.label = claim.authClaimName;
+    inp.value = claim.authClaimValue;
+
+    var del = document.createElement("md-filled-tonal-icon-button");
+    var img = document.createElement("img");
+    img.src = "/public/trashcan-logo";
+    img.alt = "Delete";
+    del.appendChild(img);
+
+    del.onclick = () => claimsContainer.removeChild(sec);
+
+    sec.appendChild(sel);
+    sec.appendChild(inp);
+    sec.appendChild(del);
+
+    return sec
+}
+
+function getNotEditableField(claim) {
+    var sec = document.createElement("section");
+    sec.classList.add("horizontalLine");
+
+    var sel = document.createElement("h3");
+    sel.textContent = claim.authClaimName + ":";
+    var inp = document.createElement("p");
+    inp.textContent = claim.authClaimValue;
+
+    sec.appendChild(sel);
+    sec.appendChild(inp);
+
+    return sec;
+}
+
 async function getMyInfo() {
     var res = await authenticatedFetch("./profile/info", {
         method: "GET",
@@ -15,47 +65,11 @@ async function getMyInfo() {
     var claimsContainer = document.getElementById("claimsContainer");
 
     payload.forEach((x) => {
-        var sec = document.createElement("section");
-        sec.classList.add("horizontalLine");
-
-        if (x.authClaimRight == 0) {
-            var sel = document.createElement("md-outlined-select");
-
-            var opt = document.createElement("md-select-option");
-            opt.textContent = "";
-            sel.appendChild(opt);
-            var opt = document.createElement("md-select-option");
-            opt.textContent = "Yes";
-            sel.appendChild(opt);
-            var opt = document.createElement("md-select-option");
-            opt.textContent = "No";
-            sel.appendChild(opt);
-
-            var inp = document.createElement("md-outlined-text-field");
-            inp.label = x.authClaimName;
-            inp.value = x.authClaimValue;
-
-            var del = document.createElement("md-filled-tonal-icon-button");
-            var img = document.createElement("img");
-            img.src = "/public/trashcan-logo";
-            img.alt = "Delete";
-            del.appendChild(img);
-
-            sec.appendChild(sel);
-            sec.appendChild(inp);
-            sec.appendChild(del);
-        }
-        else {
-            var claim = document.createElement("h3");
-            claim.textContent = x.authClaimName + ":";
-            var val = document.createElement("p");
-            val.textContent = x.authClaimValue;
-
-            sec.appendChild(claim);
-            sec.appendChild(val);
-        }
         
-        claimsContainer.appendChild(sec);
+        if (x.authClaimRight == 0)
+            claimsContainer.appendChild(getEditableField(x));
+        else
+            claimsContainer.appendChild(getNotEditableField(x));
     })
 }
 
