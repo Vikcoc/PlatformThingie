90ce07c2be6a06ac75a094ca96d7d7279fc09b9f
Victor Cociobanu <vikcoc@yahoo.com>
2024-03-30 21:08:32 +0200

    Forgotten service and to make sure db update won't drop anymore, hoping for no more breaking changes

--- UsersDbComponent/seeding/Seeder.cs
+++ UsersDbComponent/seeding/Seeder.cs
@@ -16,7 +16,6 @@ namespace UsersDbComponent.seeding
             optionsBuilder.UseNpgsql(connectionString);
             var db = new AuthContext(optionsBuilder.Options);
 
-            await db.Database.EnsureDeletedAsync();
             await db.Database.MigrateAsync();
 
             var claims = new SeedAuthClaimNames();
