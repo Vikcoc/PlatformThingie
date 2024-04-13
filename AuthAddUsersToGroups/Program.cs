41e32b2d49ba3f928decb33ab848171521fb835d
Victor Cociobanu <vikcoc@yahoo.com>
2024-04-13 21:49:21 +0300

    added config

--- AuthAddUsersToGroups/Program.cs
+++ AuthAddUsersToGroups/Program.cs
@@ -5,7 +5,6 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Configuration.Json;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using PlatformInterfaces;
 
 HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
 builder.Configuration.Sources.Add(new JsonConfigurationSource()
