26e3bc61a10c7fd3a907da74e3b549b66ab80787
Victor-Andrei Cociobanu <v.cociobanu@lipton.com>
2024-04-29 14:10:44 +0300

    Test Kafka client

--- KafkaIsOn.Dockerfile
+++ KafkaIsOn.Dockerfile
@@ -1,9 +1,23 @@
-FROM debian:stable-slim AS build
-WORKDIR /script
-COPY kafka-wait-and-topics.sh kafka-wait-and-topics.sh
-RUN chmod +x kafka-wait-and-topics.sh
+FROM mcr.microsoft.com/dotnet/aspnet:8.0 AS base
+WORKDIR /app
+EXPOSE 80
 
-FROM apache/kafka:3.7.0 as final
-WORKDIR /opt/kafka/bin
-COPY --from=build /script/kafka-wait-and-topics.sh kafka-wait-and-topics.sh
-ENTRYPOINT ["./kafka-wait-and-topics.sh"]
+FROM mcr.microsoft.com/dotnet/sdk:8.0 AS build
+WORKDIR /src
+
+# based on eshops on container
+# https://github.com/dotnet-architecture/eShopOnContainers/blob/dev/src/Services/Basket/Basket.API/Dockerfile
+# will not bother with cache yet
+
+COPY . .
+
+WORKDIR /src/ManageKafkaTopics
+RUN dotnet publish -c Release -o /app
+
+FROM build AS publish
+
+# the final 
+FROM base AS final
+WORKDIR /app
+COPY --from=publish /app .
+ENTRYPOINT ["dotnet", "ManageKafkaTopics.dll"]
---
+++ ManageKafkaTopics/Program.cs
@@ -0,0 +1,44 @@
+﻿using Confluent.Kafka;
+using Confluent.Kafka.Admin;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
+using Microsoft.Extensions.Hosting;
+
+HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
+builder.Configuration.Sources.Add(new JsonConfigurationSource()
+{
+    Path = "conf/config.json"
+});
+
+var connection = builder.Configuration["Broker"];
+var topics = builder.Configuration.GetSection("Topics").Get<string[]>();
+
+var config = new AdminClientConfig { 
+    BootstrapServers = connection,
+};
+
+var tcs = new TaskCompletionSource<bool>();
+//because it runs on a background thread
+//because fuck me i guess
+var adminClient = new AdminClientBuilder(config)
+    .SetLogHandler((a, b) => {
+        //I hope that it will always have error as the first log if error
+        Console.WriteLine(b.Message);
+        tcs.TrySetResult(b.Level != SyslogLevel.Error);
+        })
+    .Build();
+
+var res = await tcs.Task;
+
+if (!res)
+    throw new Exception("Could not work");
+    
+var newTopics = topics!.Except(adminClient.GetMetadata(new TimeSpan(0, 1, 0)).Topics.Select(x => x.Topic))
+    .Select(x => new TopicSpecification
+    {
+        Name = x,
+        NumPartitions = 1,
+        ReplicationFactor = 1
+    });
+
+await adminClient.CreateTopicsAsync(newTopics);
